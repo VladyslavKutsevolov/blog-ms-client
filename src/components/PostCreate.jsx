b1510e68472453016654b3c97725f8d5a08cc09b
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-04-05 13:17:45 -0600

    route req to posts.com

--- src/components/PostCreate.jsx
+++ src/components/PostCreate.jsx
@@ -7,7 +7,7 @@ const PostCrate = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    await axios.post("http://localhost:4000/posts", { title });
+    await axios.post("http://posts.com/posts", { title });
 
     setTitle("");
   };
