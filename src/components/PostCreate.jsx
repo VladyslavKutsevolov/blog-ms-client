78b300fccd1c372b286082b56bf662640497375e
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-04-05 13:31:01 -0600

    update route for post service

--- src/components/PostCreate.jsx
+++ src/components/PostCreate.jsx
@@ -7,7 +7,7 @@ const PostCrate = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    await axios.post("http://posts.com/posts", { title });
+    await axios.post("http://posts.com/posts/create", { title });
 
     setTitle("");
   };
